198a6eeb962105ececcc80ba6efcdfd1d86c1ee6
polosa00 <i@apolochanin.ru>
2023-08-02 14:50:21 +0300

    Initial Commit

---
+++ ToDoList_SwiftUI/ToDoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUIApp.swift
+//  ToDoList_SwiftUI
+//
+//  Created by Александр Полочанин on 2.08.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
